0b6c59ef1bb5a18441efd0dc4f1ff7523d5db5e0
NoobHubV4 <rdobo3598@gmail.com>
2025-07-03 16:54:38 +0700

    Create Wacky Bats Kill Aura.lua

---
+++ Wacky Bats Kill Aura.lua
@@ -0,0 +1,42 @@
+local Players = game.Players
+local LocalPlayer = Players.LocalPlayer
+
+function FindTool(Item)
+  if Item == "guibat" then
+    local tool = LocalPlayer.Character:FindFirstChild("Guibat") or LocalPlayer.Backpack:FindFirstChild("Guibat")
+    if tool then
+      return tool
+    end
+  elseif Item == "batburger" then
+    local tool = LocalPlayer.Character:FindFirstChild("Batburger") or LocalPlayer.Backpack:FindFirstChild("Batburger")
+    if tool then
+      return tool
+    end
+  end
+end
+
+function BuyItem(Item)
+        game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("UpdateLastBat"):FireServer(Item)
+end
+
+function KillAura()
+  local tool = FindTool("guibat")
+  if not tool then
+    BuyItem("Guibat")
+  end
+  game:GetService("ReplicatedStorage"):WaitForChild("BatRemotes"):WaitForChild("Guibat"):WaitForChild("Play"):FireServer(tool)
+end
+        
+game.RunService.Stepped:Connect(function()
+    local VPart = LocalPlayer.Character.HumanoidRootPart
+    for _, Targets in pairs(Players:GetPlayers()) do
+						if Targets ~= v and Targets.Character and not Targets.Character:FindFirstChildWhichIsA("ForceField") and Targets.Character:FindFirstChildOfClass("Humanoid").Health ~= 0 then
+							local TPart = Targets.Character:FindFirstChildWhichIsA("BasePart")
+							if VPart and TPart and Targets ~= LocalPlayer then
+								if (TPart.Position-VPart.Position).Magnitude <= 35 then
+									KillAura()
+								end
+							end
+						end
+    end
+end)
