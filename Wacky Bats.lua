74b376438a583056f7ecd22fe93be302b84194d7
NoobHubV4 <rdobo3598@gmail.com>
2025-07-03 19:23:28 +0700

    Update Wacky Bats.lua

--- Wacky Bats.lua
+++ Wacky Bats.lua
@@ -1,83 +1,83 @@
-local Workspace = game:GetService("Workspace")
-local CoreGui = game:GetService("CoreGui")
-local Players = game:GetService("Players")
-local WB = Instance.new("ScreenGui")
-local BG = Instance.new("Frame")
-local Title = Instance.new("TextLabel")
-local Toggle = Instance.new("TextButton")
-local Credit = Instance.new("TextLabel")
-local Plr = Players.LocalPlayer
-local SwingRemote = false
- 
-WB.Name = "WB"
-WB.Parent = game.CoreGui
- 
-BG.Name = "BG"
-BG.Parent = WB
-BG.BackgroundColor3 = Color3.new(0.0980392, 0.0980392, 0.0980392)
-BG.BorderColor3 = Color3.new(0.0588235, 0.0588235, 0.0588235)
-BG.BorderSizePixel = 2
-BG.Position = UDim2.new(0.149479166, 0, 0.82087779, 0)
-BG.Size = UDim2.new(0, 210, 0, 127)
-BG.Active = true
-BG.Draggable = true
- 
-Title.Name = "Title"
-Title.Parent = BG
-Title.BackgroundColor3 = Color3.new(0.266667, 0.00392157, 0.627451)
-Title.BorderColor3 = Color3.new(0.180392, 0, 0.431373)
-Title.BorderSizePixel = 2
-Title.Size = UDim2.new(0, 210, 0, 33)
-Title.Font = Enum.Font.Highway
-Title.Text = "Auto Swing"
-Title.TextColor3 = Color3.new(1, 1, 1)
-Title.FontSize = Enum.FontSize.Size32
-Title.TextSize = 30
-Title.TextStrokeColor3 = Color3.new(0.180392, 0, 0.431373)
-Title.TextStrokeTransparency = 0
- 
-Toggle.Parent = BG
-Toggle.BackgroundColor3 = Color3.new(0.266667, 0.00392157, 0.627451)
-Toggle.BorderColor3 = Color3.new(0.180392, 0, 0.431373)
-Toggle.BorderSizePixel = 2
-Toggle.Position = UDim2.new(0.152380958, 0, 0.374192119, 0)
-Toggle.Size = UDim2.new(0, 146, 0, 36)
-Toggle.Font = Enum.Font.Highway
-Toggle.FontSize = Enum.FontSize.Size28
-Toggle.Text = "Swing [OFF]"
-Toggle.TextColor3 = Color3.new(1, 1, 1)
-Toggle.TextSize = 25
-Toggle.TextStrokeColor3 = Color3.new(0.180392, 0, 0.431373)
-Toggle.TextStrokeTransparency = 0
+local Library = loadstring(Game:HttpGet('https://raw.githubusercontent.com/NoobHubV1/NoobHubV1/main/Wizard%20Lib.lua'))()
+local Players = game.Players
+local LocalPlayer = Players.LocalPlayer
+local States = {}
 
-Credit.Name = "Credit"
-Credit.Parent = BG
-Credit.BackgroundColor3 = Color3.new(1, 1, 1)
-Credit.BackgroundTransparency = 1
-Credit.Position = UDim2.new(0.195238099, 0, 0.866141737, 0)
-Credit.Size = UDim2.new(0, 128, 0, 17)
-Credit.Font = Enum.Font.SourceSans
-Credit.FontSize = Enum.FontSize.Size18
-Credit.Text = "Created by NoobHubV1"
-Credit.TextColor3 = Color3.new(1, 1, 1)
-Credit.TextSize = 16
-Credit.TextStrokeColor3 = Color3.new(0.196078, 0.196078, 0.196078)
-Credit.TextStrokeTransparency = 0
-Credit.TextWrapped = true
- 
-Toggle.MouseButton1Click:connect(function()
-    if SwingRemote == false then
-        SwingRemote = true
-        Toggle.Text = "Swing [ON]"
-        Stepped = game.RunService.Stepped:Connect(function()
-            if not SwingRemote == false then
-                    game:GetService("ReplicatedStorage"):WaitForChild("BatRemotes"):WaitForChild("Basic Bat"):FireServer(game:GetService("Players").LocalPlayer.Character:FindFirstChildOfClass("Tool"))
-            else
-                    Stepped:Disconnect()
-            end
-        end)
-    elseif SwingRemote then
-        SwingRemote = false
-        Toggle.Text = "Swing [OFF]"
+function FindTool(Item)
+  if Item == "magnetizer" then
+    local tool = LocalPlayer.Character:FindFirstChild("Magnetizer") or LocalPlayer.Backpack:FindFirstChild("Magnetizer")
+    if tool then
+      return tool
     end
+  elseif Item == "batburger" then
+    local tool = LocalPlayer.Character:FindFirstChild("Batburger") or LocalPlayer.Backpack:FindFirstChild("Batburger")
+    if tool then
+      return tool
+    end
+  end
+end
+
+function BuyItem(Item)
+        game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("UpdateLastBat"):FireServer(Item)
+end
+
+function KillAura()
+  local tool = FindTool("magnetizer")
+  if not tool then
+    BuyItem("Magnetizer")
+  end
+  game:GetService("ReplicatedStorage"):WaitForChild("BatRemotes"):WaitForChild("Magnetizer"):WaitForChild("Magnetize"):FireServer(tool)
+  LocalPlayer.Character.Humanoid.WalkSpeed = 20
+end
+
+function HealPlayer()
+	local tool = FindTool("batburger")
+	if not tool then
+		BuyItem("Batburger")
+	end
+	game:GetService("ReplicatedStorage"):WaitForChild("BatRemotes"):WaitForChild("Batburger"):WaitForChild("Snack"):FireServer(tool)
+	LocalPlayer.Character.Humanoid.WalkSpeed = 20
+end
+
+spawn(function()
+    task0 = function()
+	 if States.KillAura then
+             local VPart = LocalPlayer.Character.HumanoidRootPart
+             for _, Targets in pairs(Players:GetPlayers()) do
+	           if Targets ~= v and Targets.Character and not Targets.Character:FindFirstChildWhichIsA("ForceField") and Targets.Character:FindFirstChildOfClass("Humanoid").Health ~= 0 then
+		          local TPart = Targets.Character:FindFirstChildWhichIsA("BasePart")
+		          if VPart and TPart and Targets ~= LocalPlayer then
+			          if (TPart.Position-VPart.Position).Magnitude <= 30 then
+				         KillAura()
+			          end
+		          end
+	           end
+              end
+	 end
+	 local humanoid = LocalPlayer.Character.Humanoid
+         if humanoid.Health <= 99 and States.AutoHeal then
+	      HealPlayer()
+	 end
+	 if States.AutoSwing then
+              game:GetService("ReplicatedStorage"):WaitForChild("BatRemotes"):WaitForChild("Basic Bat"):FireServer(game:GetService("Players").LocalPlayer.Character:FindFirstChildOfClass("Tool"))
+	 end
+    end
+    while task.wait() do
+	pcall(task0)
+    end
+end)
+local Window = Library:NewWindow("NoobHubV1 Hub")
+
+local Section = Window:NewSection("Auto Farm")
+
+Section:CreateToggle("Kill Aura", function(v)
+	States.KillAura = v
+end)
+
+Section:CreateToggle("Auto Heal", function(v)
+	States.AutoHeal = v
+end)
+
+Section:CreateToggle("Auto Swing", function(v)
+	States.AutoSwing = v
 end)
