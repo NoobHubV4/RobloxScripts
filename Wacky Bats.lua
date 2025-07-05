c243bbc06105bebb382ebd7e4c2255badcd9f2fd
NoobHubV4 <rdobo3598@gmail.com>
2025-07-05 20:16:50 +0700

    Update Wacky Bats.lua

--- Wacky Bats.lua
+++ Wacky Bats.lua
@@ -37,7 +37,7 @@ local function TweenTP(cf)
 	local diff = cf.p - root.Position
 	local oldg = workspace.Gravity
 	workspace.Gravity = 0
-	for i=0,diff.Magnitude,5 do
+	for i=0,diff.Magnitude,4 do
 		root.CFrame = cf0 + diff.Unit * i
 		root.Velocity,root.RotVelocity=Vector3.new(),Vector3.new()
 		task.wait()
@@ -249,14 +249,15 @@ end
 
 function Kill(Target)
   repeat task.wait()
+	game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("UpdateMobileShiftlock"):FireServer(false)
+	game:GetService("ReplicatedStorage"):WaitForChild("BatRemotes"):WaitForChild("Basic Bat"):FireServer(FindTool())
 	targetcframe = Target.Character.HumanoidRootPart.CFrame
-	TweenTP(targetcframe * CFrame.new(0, 0, -3))
+	TweenTP(targetcframe * CFrame.new(0, 0, -2))
 	local CharPos = game.Players.LocalPlayer.Character.HumanoidRootPart.Position
 	local tpos = Target.Character:FindFirstChild("HumanoidRootPart").Position
 	local TPos = Vector3.new(tpos.X,CharPos.Y,tpos.Z)
 	local NewCFrame = CFrame.new(CharPos,TPos)
 	Players.LocalPlayer.Character.HumanoidRootPart.CFrame = NewCFrame
-	AbilityNoCooldown("Blast")
   until Target.Character.Humanoid.Health == 0 or Target.Character:FindFirstChildWhichIsA("ForceField")
 end
 
