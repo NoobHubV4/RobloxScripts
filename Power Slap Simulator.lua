663661c970647ce57389d0769df6f06ed3917540
NoobHubV4 <rdobo3598@gmail.com>
2025-07-10 06:42:52 +0700

    Update and rename Power Slap Simulator to Power Slap Simulator.lua

--- Power Slap Simulator.lua
+++ Power Slap Simulator.lua
@@ -1,7 +1,14 @@
 local Library = loadstring(Game:HttpGet('https://raw.githubusercontent.com/NoobHubV1/NoobHubV1/main/Wizard%20Lib.lua'))()
 local Window = Library:NewWindow("Power Slap Simulator")
 local Rstorage = game.ReplicatedStorage
+local player = game.Players.LocalPlayer
+local stats = player:FindFirstChild("leaderstats")
 local Main = Window:NewSection("Main")
 Main:CreateTextbox("Give Power", function(value)
      Rstorage.Remotes.Client:InvokeServer("ClickStat", tonumber(value))
+end)
+Main:CreateTextbox("Give Health", function(value)
+     if stats:FindFirstChild("Health") then
+          stats.Health.Value = tonumber(value)
+     end
 end)
