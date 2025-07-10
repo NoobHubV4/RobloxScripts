3357e3b4561cb24dd72f52c491ed4eda8a533316
NoobHubV4 <rdobo3598@gmail.com>
2025-07-10 20:49:46 +0700

    Update Dig To Earths CORE.lua

--- Dig To Earths CORE.lua
+++ Dig To Earths CORE.lua
@@ -2,24 +2,32 @@ local Library = loadstring(Game:HttpGet('https://raw.githubusercontent.com/NoobH
 local Window = Library:NewWindow("Dig To Earth's CORE")
 local Rstorage = game.ReplicatedStorage
 local States = {}
+local Pet = "Doggy"
 local Main = Window:NewSection("Main")
-Main:CreateToggle("Auto Money", function(state)
-      States.AutoMoney = state
-end)
-Main:CreateToggle("Auto Gem", function(state)
-     States.AutoGem = state
-end)
-Main:CreateToggle("Auto Chest", function(state)
-     States.AutoChest = state
-end)
+local Toggle = Window:NewSection("Toggles")
 Main:CreateButton("Get 10 Spins", function()
-     Ratorage.Remotes.SpinPrizeEvent:FireServer(10)
-end)
-Main:CreateButton("Get Dominus Pet", function()
-     Ratorage.Remotes.SpinPrizeEvent:FireServer(4)
+     Rstorage.Remotes.SpinPrizeEvent:FireServer(10)
 end)
 Main:CreateButton("Get 10X Money", function()
-     Ratorage.Remotes.SpinPrizeEvent:FireServer(8)
+     Rstorage.Remotes.SpinPrizeEvent:FireServer(8)
+end)
+Main:CreateDropdown("Pet", {"Doggy","Wizard","Cactus","Swimmer","Astronaut","Police","Triple Dominus","Cowboy","Camel","Robot","Cosmo Boy","Princess Cat","Star Gepard"}, 1, function(select)
+     Pet = select
+end)
+Main:CreateButton("Give Pet", function()
+     Rstorage.Remotes.PetCageEvent:FireServer(Pet)
+end)
+Toggle:CreateToggle("Auto Money", function(state)
+      States.AutoMoney = state
+end)
+Toggle:CreateToggle("Auto Gem", function(state)
+     States.AutoGem = state
+end)
+Toggle:CreateToggle("Auto Chest", function(state)
+     States.AutoChest = state
+end)
+Toggle:CreateToggle("Auto Win", function(state)
+     States.AutoWin = state
 end)
 spawn(function()
     task0 = function()
@@ -30,7 +38,10 @@ spawn(function()
         Rstorage.Remotes.GemEvent:FireServer(20, "bye")
       end
       if States.AutoChest then
-        Rstorage.Remotes.TreasureEvent:FireServer("Chest")
+        Rstorage.Remotes.TreasureEvent:FireServer("Chest15")
+      end
+      if States.AutoWin then
+        Rstorage.Remotes.TreasureEvent:FireServer("Cup15")
       end
     end
     while task.wait() do
