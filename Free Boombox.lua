f0e6841b4807922eb25e97af0ae72b8f1070f740
NoobHubV4 <rdobo3598@gmail.com>
2025-07-13 15:39:26 +0700

    Update Free Boombox.lua

--- Free Boombox.lua
+++ Free Boombox.lua
@@ -18,7 +18,7 @@ ScreenGui.ResetOnSpawn = true
 Frame.Parent = ScreenGui
 Frame.BackgroundColor3 = Color3.fromRGB(0, 85, 255)
 Frame.Position = UDim2.new(0.0799492374, 0, 0.483815432, 0)
-Frame.Size = UDim2.new(0, 292, 0, 171)
+Frame.Size = UDim2.new(0, 252, 0, 126)
 Frame.Active = true
 Frame.Archivable = true
 Frame.Draggable = true
@@ -27,7 +27,7 @@ TextLabel.Parent = Frame
 TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 TextLabel.BackgroundTransparency = 1.000
 TextLabel.Position = UDim2.new(0.157534242, 0, 0, 0)
-TextLabel.Size = UDim2.new(0, 200, 0, 50)
+TextLabel.Size = UDim2.new(0, 180, 0, 30)
 TextLabel.Font = Enum.Font.SourceSans
 TextLabel.Text = "Free Boombox Dupe"
 TextLabel.TextColor3 = Color3.fromRGB(0, 0, 0)
@@ -39,12 +39,12 @@ TextLabel.TextWrapped = true
 TextBox.Parent = Frame
 TextBox.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 TextBox.Position = UDim2.new(0.157534242, 0, 0.350877196, 0)
-TextBox.Size = UDim2.new(0, 200, 0, 50)
+TextBox.Size = UDim2.new(0, 180, 0, 30)
 TextBox.Font = Enum.Font.SourceSans
 TextBox.Text = ""
 TextBox.TextColor3 = Color3.fromRGB(0, 0, 0)
 TextBox.TextScaled = true
-TextBox.TextSize = 14.000
+TextBox.TextSize = 12.000
 TextBox.TextWrapped = true
 TextBox.ClearTextOnFocus = false
 TextBox.PlaceholderText = "Dupe Amount"
@@ -52,12 +52,12 @@ TextBox.PlaceholderText = "Dupe Amount"
 TextButton.Parent = Frame
 TextButton.BackgroundColor3 = Color3.fromRGB(170, 0, 255)
 TextButton.Position = UDim2.new(0.311643839, 0, 0.713450253, 0)
-TextButton.Size = UDim2.new(0, 109, 0, 39)
+TextButton.Size = UDim2.new(0, 89, 0, 19)
 TextButton.Font = Enum.Font.SourceSans
 TextButton.Text = "Dupe"
 TextButton.TextColor3 = Color3.fromRGB(0, 0, 0)
 TextButton.TextScaled = true
-TextButton.TextSize = 14.000
+TextButton.TextSize = 12.000
 TextButton.TextWrapped = true
 
 local Players = game:GetService("Players")
@@ -65,6 +65,8 @@ local Players = game:GetService("Players")
 local localPlayer = Players.LocalPlayer
 local Amount
 local dupesDone = 0
+local active = nil
+local auto = nil
 
 local function teleportCharacter(cframe)
     local char = localPlayer.Character
@@ -83,11 +85,18 @@ function Notif(Title, Text, Time)
 end
 
 TextButton.MouseButton1Click:Connect(function()
+  if not active then
+  active = true
+  saved = localPlayer.Character.HumanoidRootPart.CFrame
   Amount = tonumber(TextBox.Text)
-  while wait() do
+  auto = true
+  while auto and wait() do
         if dupesDone >= Amount then
             dupesDone = 0
             Amount = 0
+            active = false
+            auto = false
+            localPlayer.Character.HumanoidRootPart.CFrame = saved
             break
         end
 
@@ -99,9 +108,7 @@ TextButton.MouseButton1Click:Connect(function()
                 tool.Parent = localPlayer.Character
             end
          end
-        task.wait(.1)
-        game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored = true
-        task.wait(.2)
+        task.wait(.15)
         for _, tool in pairs(localPlayer.Character:GetChildren()) do
             if tool:IsA("Tool") and tool:FindFirstChild("Handle") then
                 if tool.Name == "BoomBox" then
@@ -128,10 +135,12 @@ TextButton.MouseButton1Click:Connect(function()
                  end
              end
          end
-            
-         dupesDone = dupesDone + 1     
+         dupesDone = dupesDone + 1
          task.wait(.1)
          Notif("Successfully", "Dupe: "..dupesDone)
+    end
+    elseif active then
+         Notif("Error", "Script Active, wait a end", 5)
     end
 end)
 --Player not steal boombox
